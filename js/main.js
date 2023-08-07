7d7be8063baf1aa3a6375b141e56fb54437d58c8
cblake003 <cameronburtner@gmail.com>
2023-08-07 16:00:35 -0700

    added state variables

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
 /*----- constants -----*/
-const PLAYERS {
-    "1": ""
-    "-1": ""
-    "0": null
-}
-
 const suits = ['s', 'c', 'd', 'h']
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'Q', 'K', 'A']
-// Should I make separate ranks for 10 and face cards?
-
 const originalDeck = buildOriginalDeck();
+
+/*----- state variables -----*/
+
+let SCORES {
+// player, dealer, and tie/push variables need to be established. Put in object so we can access via key and value and it'll be easier to read
+    p: 0,
+    d: 0,
+    t: 0
+}
+
+// We now need to go with what the player ended up with and what the dealer ended up with
+// Calling this variable Results
+
+let results = {
+// Need to set up a wireframe with what they're starting with. Need to figure out how to calculate results here.
+// I want to have the player results be 2 cards with each suit and value, same for computer
+    //Can I just create a result of card1 + card2 for the results? Maybe I can make a randomCardPull const that combines the suits and ranks? 
+    p: suit['h'],rank[Q] + suit['d'], rank[A]
+    d: suit['c'],rank['10'] + suit['s'], rank[09]
+// Should I make separate ranks for 10 and face cards? 
+}
+
+// This will be our winner variable, but let's call it Outcomes since there can be 5 outcomes in BJ
+// player wins, player wins w/ blackjack, dealer wins, dealer wins w/ BJ, or tie
+let outcome = 'p', 'pBJ', 'd', 'dBJ', 't'
+
 renderDeckInContainer(originalDeck, document.getElementById('original-deck-container'));
 
-	/*----- state variables -----*/
 let table
 let points
 let winner
@@ -26,7 +43,23 @@ document.querySelector('button').addEventListener('click', renderNewShuffledDeck
 
 	/*----- functions -----*/
 
-function buildOriginalDeck() {
+    init();
+    
+    function init() {
+        render();
+        
+    }
+    
+    render () {
+        renderOriginalDeck();
+        renderShuffledDeck();
+        renderResults();
+        handleChoice();
+        getwinner();
+    }
+
+
+function renderOriginalDeck() {
     const deck = [];
     suits.forEach(function(suit) {
         ranks.forEach(function(rank) {
@@ -52,7 +85,14 @@ function buildOriginalDeck() {
         };
     }:
 
-    render () {
-        renderShuffledDeck();
-        
+    renderResults () {
+
+    }
+
+    handleChoice() {
+
+    }
+
+    getWinner() {
+
     }
